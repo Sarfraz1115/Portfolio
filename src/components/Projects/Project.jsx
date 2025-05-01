9efc2d1f1cd8047bfdf3c6fb806efddd08b942b1
Sarfraz1115 <syedsarfraz907@gmail.com>
2025-05-01 13:55:21 +0530

    third Commit

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -17,8 +17,8 @@ const Project = () => {
                             <div className="photo"><img className='news' src="./news-portal.png" alt="" /></div>
                             <h2>MyNews</h2>
                             <div className="btns">
-                            <a href="https://google.com" target='_blank' className='git'>Github</a>
-                            <a href="" className='live'>Live demo</a>
+                            <a href="https://github.com/Sarfraz1115/Tic-Tac-Toe-game" target='_blank' className='git'>Github</a>
+                            <a href="https://tic-tac-toe-game-kappa-wine.vercel.app/" className='live'>Live demo</a>
                             </div>
                         </div>
                         <div className="first-about">
@@ -33,8 +33,8 @@ const Project = () => {
                             <div className="photo"><img src="./tic.jpg" alt="" /></div>
                             <h2>TicTacToe</h2>
                             <div className="btns">
-                                <a href="https://google.com" target='_blank' className='git'>Github</a>
-                                <a href="" className='live'>Live demo</a>
+                                <a href="https://github.com/Sarfraz1115/Tic-Tac-Toe-game" target='_blank' className='git'>Github</a>
+                                <a href="https://tic-tac-toe-game-kappa-wine.vercel.app/" className='live'>Live demo</a>
                             </div>
                         </div>
                         
